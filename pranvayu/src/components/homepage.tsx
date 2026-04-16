8f5bd99b9d4d208f32d5b9984164f84f918ee3e8
perinbaraj777 <perinbaraj777@gmail.com>
2026-04-16 12:42:32 +0530

    home page

---
+++ pranvayu/src/components/homepage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <>      <div className="flex flex-col items-center justify-center h-screen">   
+    </div>         
+</>
+  )
+};
